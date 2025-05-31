62349f41190c3aa41850a7e4664bb9e71b5fc41a
sonpai <sofiako2003@gmail.com>
2025-05-31 18:04:59 +0300

    inv works - 10

--- Assets/UIInventoryItem.cs
+++ Assets/UIInventoryItem.cs
@@ -1,3 +1,91 @@
+
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class UIInventoryItem : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField]
+    private Image itemImage;
+    [SerializeField]
+    private TMP_Text quantityTxt;
+    [SerializeField]
+    private Image borderImage;
+
+    public event Action<UIInventoryItem> OnItemClicked,
+        OnItemDroppedOn, OnItemBeginDrag, OnItemEndDrag,
+        OnRightMouseBtnClick;
+
+    private bool empty = true;
+
+    private void Awake()
+    {
+        ResetData();
+        Deselect();
+    }
+
+    public void ResetData()
+    {
+        this.itemImage.gameObject.SetActive(false);
+        this.quantityTxt.text = "";
+        empty = true;
+    }
+
+    public void Deselect()
+    {
+        borderImage.enabled = false;
+    }
+
+    public void SetData(Sprite sprite, int quantity)
+    {
+        this.itemImage.gameObject.SetActive(true);
+        this.itemImage.sprite = sprite;
+        this.quantityTxt.text = quantity.ToString();
+        empty = false;
+    }
+
+    public void Select()
+    {
+        borderImage.enabled = true;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightMouseBtnClick?.Invoke(this);
+        }
+        else
+        {
+            OnItemClicked?.Invoke(this);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (empty) return;
+        Debug.Log("Drag started");
+        OnItemBeginDrag?.Invoke(this);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Optional: Add functionality for visual feedback during dragging.
+        Debug.Log("Dragging...");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("Drag ended");
+        OnItemEndDrag?.Invoke(this);
+    }
+}
+
+
+
+
 //using System.Collections;
 //using System.Collections.Generic;
 //using UnityEngine;
@@ -89,87 +177,3 @@
 
 //}
 
-
-using System;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-using UnityEngine.EventSystems;
-
-public class UIInventoryItem : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
-{
-    [SerializeField]
-    private Image itemImage;
-    [SerializeField]
-    private TMP_Text quantityTxt;
-    [SerializeField]
-    private Image borderImage;
-
-    public event Action<UIInventoryItem> OnItemClicked,
-        OnItemDroppedOn, OnItemBeginDrag, OnItemEndDrag,
-        OnRightMouseBtnClick;
-
-    private bool empty = true;
-
-    private void Awake()
-    {
-        ResetData();
-        Deselect();
-    }
-
-    public void ResetData()
-    {
-        this.itemImage.gameObject.SetActive(false);
-        this.quantityTxt.text = "";
-        empty = true;
-    }
-
-    public void Deselect()
-    {
-        borderImage.enabled = false;
-    }
-
-    public void SetData(Sprite sprite, int quantity)
-    {
-        this.itemImage.gameObject.SetActive(true);
-        this.itemImage.sprite = sprite;
-        this.quantityTxt.text = quantity.ToString();
-        empty = false;
-    }
-
-    public void Select()
-    {
-        borderImage.enabled = true;
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if (eventData.button == PointerEventData.InputButton.Right)
-        {
-            OnRightMouseBtnClick?.Invoke(this);
-        }
-        else
-        {
-            OnItemClicked?.Invoke(this);
-        }
-    }
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        if (empty) return;
-        Debug.Log("Drag started");
-        OnItemBeginDrag?.Invoke(this);
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        // Optional: Add functionality for visual feedback during dragging.
-        Debug.Log("Dragging...");
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("Drag ended");
-        OnItemEndDrag?.Invoke(this);
-    }
-}
--- Assets/UIInventoryPage.cs
+++ Assets/UIInventoryPage.cs
@@ -50,7 +50,6 @@ public class UIInventoryPage : MonoBehaviour
 
     private void HandleBeginDrag(UIInventoryItem inventoryItemUI)
     {
-        Debug.Log("hi");
 
         mouseFollower.Toggle(true);
         mouseFollower.SetData(image, quantity);
